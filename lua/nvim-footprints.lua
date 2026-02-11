b3638423ed9d51df9bdf373d3992ad08d9e213dd
Alex Lebedev <alexey.broadcast@gmail.com>
2026-02-11 15:24:38 +0300

    It Works!

--- lua/nvim-footprints.lua
+++ lua/nvim-footprints.lua
@@ -126,4 +126,99 @@ function M.SetHistoryDepth(new_depth)
   run_update_matches()
 end
 
+local augroup = vim.api.nvim_create_augroup("footprints", { clear = true })
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  group = augroup,
+  callback = function() M.FootprintsInit() end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup,
+  callback = function() M.OnFiletypeSet() end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = augroup,
+  callback = function() M.OnBufEnter() end,
+})
+
+vim.api.nvim_create_autocmd("CursorMoved", {
+  group = augroup,
+  callback = function() M.OnCursorMove() end,
+})
+
+vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "FileType", "BufEnter", "BufLeave" }, {
+  group = augroup,
+  callback = function() M.Footprints() end,
+})
+
+-- Commands
+vim.api.nvim_create_user_command(
+    "FootprintsDisable",
+    function() M.FootprintsDisable() end,
+    { desc = "Disable footprints globally" }
+)
+
+vim.api.nvim_create_user_command(
+    "FootprintsEnable",
+    function() M.FootprintsEnable() end,
+    { desc = "Enable footprints globally" }
+)
+
+vim.api.nvim_create_user_command(
+    "FootprintsToggle",
+    function() M.FootprintsToggle() end,
+    { desc = "Toggle footprints globally" }
+)
+
+vim.api.nvim_create_user_command(
+    "FootprintsBufferDisable",
+    function() M.FootprintsDisable(true) end,
+    { desc = "Disable footprints for current buffer" }
+)
+
+vim.api.nvim_create_user_command(
+    "FootprintsBufferEnable",
+    function() M.FootprintsEnable(true) end,
+    { desc = "Enable footprints for current buffer" }
+)
+
+vim.api.nvim_create_user_command(
+    "FootprintsBufferToggle",
+    function() M.FootprintsToggle(true) end,
+    { desc = "Toggle footprints for current buffer" }
+)
+
+vim.api.nvim_create_user_command(
+    "FootprintsDisableCurrentLine",
+    function() M.FootprintsDisableCurrentLine() end,
+    { desc = "Disable footprints for current line" }
+)
+
+vim.api.nvim_create_user_command(
+    "FootprintsEnableCurrentLine",
+    function() M.FootprintsEnableCurrentLine() end,
+    { desc = "Enable footprints for current line" }
+)
+
+vim.api.nvim_create_user_command(
+    "FootprintsToggleCurrentLine",
+    function() M.FootprintsToggleCurrentLine() end,
+    { desc = "Toggle footprints for current line" }
+)
+
+function M.setup(opts)
+  vim.g.footprintsHistoryDepth = opts.footprintsHistoryDepth or 20
+  vim.g.footprintsExcludeFiletypes = opts.footprintsExcludeFiletypes or {"magit", "nerdtree", "diff"}
+  vim.g.footprintsEasingFunction = opts.footprintsEasingFunction or "easeinout"
+  vim.g.footprintsEnabledByDefault = opts.footprintsEnabledByDefault ~= false
+  vim.g.footprintsOnCurrentLine = opts.footprintsOnCurrentLine or false
+  vim.g.footprintsColor = opts.footprintsColor or (vim.o.background == "dark" and "#3A3A3A" or "#C1C1C1")
+  vim.g.footprintsTermColor = opts.footprintsTermColor or "208"
+
+  M.FootprintsInit()
+end
+
 return M
